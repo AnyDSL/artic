8b599ebd276b61aa2759cc0bdcf7e910b507adce
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2018-03-02 16:26:55 +0100

    Add version option

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,42 @@ inline bool should_colorize() {
 static void usage() {
     std::cout << "Usage: artic [options] files...\n"
                  "Available options:\n"
+                 "          --version  Displays the version number\n"
                  "    -h    --help     Displays this message\n";
+}
+
+static void version() {
+    static const char* version = "0.0.1";
+    static const char day[] = { __DATE__[4] == ' ' ? '0' : __DATE__[4], __DATE__[5], 0 };
+    static const char* month =
+        (__DATE__[0] == 'J' && __DATE__[1] == 'a' && __DATE__[2] == 'n') ? "01" :
+        (__DATE__[0] == 'F' && __DATE__[1] == 'e' && __DATE__[2] == 'b') ? "02" :
+        (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'r') ? "03" :
+        (__DATE__[0] == 'A' && __DATE__[1] == 'p' && __DATE__[2] == 'r') ? "04" :
+        (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'y') ? "05" :
+        (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'n') ? "06" :
+        (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'l') ? "07" :
+        (__DATE__[0] == 'A' && __DATE__[1] == 'u' && __DATE__[2] == 'g') ? "08" :
+        (__DATE__[0] == 'S' && __DATE__[1] == 'e' && __DATE__[2] == 'p') ? "09" :
+        (__DATE__[0] == 'O' && __DATE__[1] == 'c' && __DATE__[2] == 't') ? "10" :
+        (__DATE__[0] == 'N' && __DATE__[1] == 'o' && __DATE__[2] == 'v') ? "11" :
+        (__DATE__[0] == 'D' && __DATE__[1] == 'e' && __DATE__[2] == 'c') ? "12" :
+        "??";
+    static const char year[] = { __DATE__[7], __DATE__[8], __DATE__[9], __DATE__[10], 0 };
+#ifdef NDEBUG
+    static const char* build = "Release";
+#else
+    static const char* build = "Debug";
+#endif
+    std::cout << "artic " << version << " " << year << "-" << month << "-" << day <<  " (" << build << ")\n";
 }
 
 struct ProgramOptions {
     std::vector<std::string> files;
+
+    inline bool matches(const char* a, const char* opt) {
+        return !strcmp(a, opt);
+    }
 
     inline bool matches(const char* a, const char* opt1, const char* opt2) {
         return !strcmp(a, opt1) || !strcmp(a, opt2);
@@ -53,6 +84,9 @@ struct ProgramOptions {
             if (argv[i][0] == '-') {
                 if (matches(argv[i], "-h", "--help")) {
                     usage();
+                    return false;
+                } else if (matches(argv[i], "--version")) {
+                    version();
                     return false;
                 } else {
                     log::error("Unknown option '{}'", argv[i]);
