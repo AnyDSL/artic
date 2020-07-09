7a35ac7481005b56d2f7efbd9b9808dc1204c870
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2020-07-09 13:16:27 +0200

    Implement explicit address computation for constant

--- src/emit.cpp
+++ src/emit.cpp
@@ -451,6 +451,16 @@ const thorin::Def* Emitter::load(const thorin::Def* ptr, thorin::Debug debug) {
     return world.extract(pair, thorin::u32(1));
 }
 
+const thorin::Def* Emitter::addr_of(const thorin::Def* def, bool is_constant, thorin::Debug debug) {
+    if (is_constant) {
+        return world.global(def, false, debug);
+    } else {
+        auto ptr = alloc(def->type(), debug);
+        store(ptr, def, debug);
+        return ptr;
+    }
+}
+
 const thorin::Def* Emitter::emit(const ast::Node& node) {
     if (node.def)
         return node.def;
@@ -788,8 +798,12 @@ const thorin::Def* ReturnExpr::emit(Emitter&) const {
 const thorin::Def* UnaryExpr::emit(Emitter& emitter) const {
     const thorin::Def* op = nullptr;
     const thorin::Def* ptr = nullptr;
-    if (tag == AddrOf)
-        return emitter.emit(*arg);
+    if (tag == AddrOf || tag == AddrOfMut) {
+        auto def = emitter.emit(*arg);
+        if (arg->type->isa<RefType>())
+            return def;
+        return emitter.addr_of(def, arg->is_constant(), debug_info(*this));
+    }
     if (is_inc() || is_dec()) {
         ptr = emitter.emit(*arg);
         op  = emitter.load(ptr, debug_info(*this));
@@ -924,13 +938,7 @@ const thorin::Def* ImplicitCastExpr::emit(Emitter& emitter) const {
     if (auto ptr_type = this->type->isa<artic::PtrType>();
         ptr_type && type->subtype(ptr_type->pointee))
     {
-        if (expr->is_constant()) {
-            def = emitter.world.global(def, false, debug_info(*this));
-        } else {
-            auto ptr = emitter.alloc(def->type(), debug_info(*this));
-            emitter.store(ptr, def, debug_info(*this));
-            def = ptr;
-        }
+        def = emitter.addr_of(def, expr->is_constant(), debug_info(*this));
     }
     // This handles conversion between pointer types (e.g. &[i32 * 4] to &[i32])
     // and conversions between other types and the no-return type.
