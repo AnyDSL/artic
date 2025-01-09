7f1f4604ccd8afdcd67d95aa3e4669713e6fa1b9
Hugo Devillers <devillers@cg.uni-saarland.de>
2025-01-09 14:21:48 +0100

    arena_ptr fixes

--- include/artic/arena.h
+++ include/artic/arena.h
@@ -11,18 +11,18 @@ struct arena_ptr {
     arena_ptr() : _ptr(nullptr) {}
     arena_ptr(T* ptr) : _ptr(ptr) {}
 
-    template<typename S, std::enable_if_t<std::is_convertible_v<S*, T*>, bool> = true>
-    arena_ptr(arena_ptr<S>& other) : _ptr(static_cast<S*>(other._ptr)) {}
+    // template<typename S, std::enable_if_t<std::is_convertible_v<S*, T*>, bool> = true>
+    // arena_ptr(arena_ptr<S>& other) : _ptr(other._ptr) {}
 
     template<typename S, std::enable_if_t<std::is_convertible_v<S*, T*>, bool> = true>
-    arena_ptr(arena_ptr<S>&& other) : _ptr(static_cast<S*>(other._ptr)) {
+    arena_ptr(arena_ptr<S>&& other) : _ptr(other._ptr) {
         other._ptr = nullptr;
     }
     ~arena_ptr() {
         _ptr = nullptr;
     }
 
-    arena_ptr<T>& operator=(const arena_ptr<T>& other) { _ptr = other._ptr; return *this; }
+    // arena_ptr<T>& operator=(const arena_ptr<T>& other) { _ptr = other._ptr; return *this; }
     arena_ptr<T>& operator=(arena_ptr<T>&& other) { _ptr = other._ptr; other._ptr = nullptr; return *this; }
 
     T* operator->() const { return _ptr; }
